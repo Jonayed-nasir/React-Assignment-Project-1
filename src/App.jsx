6f8ebadafbb72eec32c76c05a66a9d4f42f8ad87
jonayed-nasir <junayednasir073@gmail.com>
2025-08-20 12:20:52 +0600

    wizia prjoect hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Button } from '@heroui/react';
 import './App.css';
 import NavBar from './components/Navbar';
-
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
     <main className='overflow-x-hidden'>
       <NavBar/>
+      <Hero/>
       <div className='w-full max-w-[1440px] mx-auto px-3 md:px-10 lg:px-20 xl:px-24 py-5'>
       </div>
     </main>
